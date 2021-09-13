49f033b504dd733f155096b7ba90cebc665e3492
Rafael Ribeiro Bernardes <rafaelribeiro@MacBook-Pro-de-Rafael.local>
2021-09-13 12:11:38 -0300

    Treinamento CONST LET E VAR

---
+++ scrpt.js
@@ -0,0 +1,14 @@
+
+
+var numero1 = 4;
+let numero2 = 5;
+const numero3 = true;
+
+// numero1 = 7; Var fica acessível em qualquer lugar (variaveis globais)
+// numero3 = 8; Const nao pode ser atribuida (variaveis locais)
+// numero3.nome = "Rafael"; (variaveis locais)
+
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
